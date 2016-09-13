63b7347b5c8722646c9d6b6c7068fa539957eb4e
Matías Muñoz Espinoza <writkas2@gmail.com>
2016-09-12 23:13:03 -0300

    Primer commit

---
+++ assets/music/README.md
@@ -0,0 +1,1 @@
+La música no esta bajo licencia GPL, estan bajo otras licencias.
---
+++ scripts/save.gd
@@ -0,0 +1,9 @@
+
+extends Node
+
+const PATH_GLOBAL_DATA = "user://global_data"
+
+func create_global_data():
+	var save_dict = {
+		first_time = true
+	}
